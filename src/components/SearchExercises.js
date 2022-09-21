cb868c258949b13e684559683360fd3a3eaa1b0f
manases <manasesgil95@gmail.com>
2022-09-21 16:15:39 -0500

    working on search bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,8 +3,10 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <Stack>
-       Hi
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
     </Stack>
   )
 }
