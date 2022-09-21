16e4bb58e160c13b6d0413c26f0c403ba2a66f75
manases <manasesgil95@gmail.com>
2022-09-21 14:34:45 -0500

    got navbar and stric mode on index.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Stack } from '@mui/system';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar() {
-  return (
-    <div>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          {/* <Link to='/profile'><img src={profile.id}></img></Link> */}
-          <Link to='/' >
-            Home 
-          </Link>
-          <Link to='/myworkouts'>
-            My workouts
-          </Link>
-            </div>
-        </nav>
-    </div>
-  )
-}
+import Logo from '../assets/images/Logo.png';
 
-export default Navbar
+const Navbar = () => (
+  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+    <Link to="/">
+      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+    </Link>
+    <Stack
+      direction="row"
+      gap="40px"
+      fontFamily="Alegreya"
+      fontSize="24px"
+      alignItems="flex-end"
+    >
+      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+    </Stack>
+  </Stack>
+);
+
+export default Navbar;
