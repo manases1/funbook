86f9c81aa799c3a3014d87a47dded405166480b5
manases <manasesgil95@gmail.com>
2022-09-21 16:04:33 -0500

    hero banner styling and firness message

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,8 +4,8 @@ import { Box, Stack, Typography } from '@mui/material';
 import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
-  <Box>
-    Fitness club
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
   </Box>
 );
 
